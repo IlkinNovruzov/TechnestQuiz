4bdabd20278c5679d4f6617d769fbed99a799d2d
IlkinNovruzov <inovruzov2004@gmail.com>
2024-07-24 20:02:44 +0400

    update

--- TechnestQuiz/Controllers/BooksController.cs
+++ TechnestQuiz/Controllers/BooksController.cs
@@ -1,11 +1,54 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TechnestQuiz.Handlers;
+using TechnestQuiz.Models;
 
 namespace TechnestQuiz.Controllers
 {
-    [Route("api/[controller]")]
     [ApiController]
+    [Route("api/[controller]")]
     public class BooksController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public BooksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<long>> Create(CreateBookCommand command)
+        {
+            var bookId = await _mediator.Send(command);
+            return Ok(bookId);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, UpdateBookCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(long id)
+        {
+            await _mediator.Send(new DeleteBookCommand { Id = id });
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> Get(long id)
+        {
+            var book = await _mediator.Send(new GetBookQuery { Id = id });
+            return Ok(book);
+        }
     }
+
 }
--- TechnestQuiz/Program.cs
+++ TechnestQuiz/Program.cs
@@ -1,4 +1,7 @@
 
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TechnestQuiz.Models;
 namespace TechnestQuiz
 {
     public class Program
@@ -8,7 +11,10 @@ namespace TechnestQuiz
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+            builder.Services.AddMediatR(typeof(Program));
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
